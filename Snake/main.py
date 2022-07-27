f89aa660b971eb63a5e993e1736ba180bebd4125
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2022-07-27 09:05:16 +0200

    Color and text test

--- Snake/main.py
+++ Snake/main.py
@@ -1,14 +1,27 @@
 import pygame
 from pygame.locals import *
 
+RESOLUTION = (1280, 720)
+DIFFICULTY = 1
+
+Color_white = (255, 255, 255)
+Color_Black = (0, 0, 0)
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1280, 720), pygame.SCALED)
     pygame.display.set_caption("Test Window")
 
+    width = screen.get_width()
+    height = screen.get_height()
+
+    font = pygame.font.SysFont('Corbel', 35)
+
+    test = font.render('quit' , True , Color_Black)
+
     clock = pygame.time.Clock()
     live = True
-    x = 0
 
     while live:
         # Set clock tick to 60 fps
@@ -18,12 +31,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 live = False
-
-        # Change background color
-        if x == 255: x = 0
-        else: x += 1
-        screen.fill((x, 0, 0))
         
+        screen.fill(Color_white)
+        screen.blit(test , (width/2,height/2))
+
         # Draw Scene
         pygame.display.flip()
 
