9949379074b4f39588b7a2adcf898394c8f24534
Nicolas Sudres <nicolas.sudres@hotmail.fr>
2022-08-01 01:26:50 +0200

    add comment and print snake on the matrix

--- Snake/src/snake.py
+++ Snake/src/snake.py
@@ -33,28 +33,37 @@ def Snake_move(M, L, dir):
     Fruit = False
     List_save = L
     Save_Value = L.val
-    Next_pos = (DIRECTION[dir][0] + L.val[0], DIRECTION[dir][1] + L.val[1])
+    
+    # Calculate new snake head coord
+    Next_coord = (DIRECTION[dir][0] + L.val[0], DIRECTION[dir][1] + L.val[1])
 
-    if M[Next_pos[0]][Next_pos[1]] == 3:
+    # Check nature of the future snake head coord
+    if M[Next_coord[0]][Next_coord[1]] == 3:
         Fruit = True
-        M[Next_pos[0]][Next_pos[1]] == 0
-    elif M[Next_pos[0]][Next_pos[1]] == 2: 
+        M[Next_coord[0]][Next_coord[1]] == 0
+    elif M[Next_coord[0]][Next_coord[1]] == 2: 
         raise ValueError(1)         # Error : You take a wall !
         quit
-    elif M[Next_pos[0]][Next_pos[1]] == 1:
+    elif M[Next_coord[0]][Next_coord[1]] == 1:
         raise ValueError(2)         # Error : You ate your tale !
         quit
 
-    L.val = Next_pos
+    # Update snake head coord and print on matrix
+    L.val = Next_coord
+    M[Next_coord[0]][Next_coord[1]] = 1
     L = L.next
 
+    # Loot to update snake segment coord
     while L is not None:
         Save_bis_Value = L.val
         L.val = Save_Value
         Save_Value = Save_bis_Value
         L = L.next
     
+    # If snake eat fruit -> spawn new segment at the end of the snake
     if Fruit:
         List_save = List.List_add(List_save, Save_Value)
+    else:
+        M[Save_Value[0]][Save_Value[1]] = 0
 
-    return List_save
+    return List_save, M
